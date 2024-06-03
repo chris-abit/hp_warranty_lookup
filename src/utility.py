c8194c24000eb426441e03f26980d53b19dbc675
Christopher A <cau005@uit.no>
2024-06-03 13:17:05 +0200

    Creating basic function for writing warranty results to file.

--- src/utility.py
+++ src/utility.py
@@ -8,11 +8,24 @@ def computers_batched(computers):
     """
     max_items = 15
     n_items = len(computers)
+    if n_items < 2:
+        raise ValueError("A minimum of two computers is required.")
     slizes = [[i, i + max_items] for i in range(0, n_items, max_items)]
-    if n_items % max_items < 2:
+    if n_items % max_items == 1:
         last_slize = slizes[-1]
         second_last = slizes[-2]
         second_last[1] -= 1
         last_slize[0] -= 1
     computer_batches = [computers[start:stop] for start, stop in slizes]
     return computer_batches
+
+
+def write_results(fname, computers, file_create):
+    """
+    Write results from warranty lookup to file.
+    Will create a truncated file if append is false.
+    """
+    if file_create:
+        computers.to_csv(fname, header=False, index=False, mode="a")
+    else:
+        computers.to_csv(fname, index=False)
