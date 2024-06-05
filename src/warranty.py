a0bf408ac4b7920997eb7e3396e7bc049454dc95
Christopher A <cau005@uit.no>
2024-06-05 14:03:45 +0200

    Writing functionality for sending product information related to hp warranty lookup.

--- src/warranty.py
+++ src/warranty.py
@@ -78,29 +78,144 @@ def serial_numbers_send(driver, computers):
     """
     Fill serial numbers into hp warranty lookup page.
     """
-    submit = driver.find_element(By.ID, "FindMyProduct")
     sn_field = "inputtextpfinder"
     sn_fields = [f"{sn_field}{i}" for i in range(1, len(computers))]
     sn_fields.insert(0, sn_field)
     computers["sn_field"] = sn_fields
-    func = lambda x: send_keys(driver, "sn_field", "serial_number")
-    computers.apply(func)
-    submit.click()
+    func = lambda x: send_keys(driver, x, "sn_field", "serial_number")
+    computers.apply(func, axis=1)
+    submit_form(driver)
+
+
+def submit_form(driver):
+    """
+    Submit hp warranty lookup form.
+    Hp has three different id's for the submit button, which
+    depends on the context.
+    """
+    variations = [
+        "FindMyProduct",
+        "FindMyProductNumber",
+        "FindMyProductNumber-multiple",
+    ]
+    elem = None
+    for var in variations:
+        string = f'"{var}"'
+        if string in driver.page_source:
+            elem = driver.find_element(By.ID, var)
+    if not elem:
+        raise ValueError("No submit button found in page!")
+    elem.click()
 
 
 def product_numbers_send(driver, computers):
     """
     Fill product numbers into hp warranty lookup page.
     """
-    submit = driver.find_element(By.ID, "FindMyProductNumber-multiple")
     max_items = len(computers) + 1
     pn_field = "product-number inputtextPN"
     pn_fields = [f"{pn_field}{i}" for i in range(1, max_items)]
     computers["pn_field"] = pn_fields
-    computers = computers[computers["pn_field"] in driver.page_source]
+    pn_fields = filter(lambda x: x in driver.page_source, pn_fields)
+    missing = computers[computers["pn_field"].isin(pn_fields)]
     func = lambda x: send_keys(driver, x, "pn_field", "product_number")
-    computers.apply(func)
-    submit.click()
+    missing.apply(func, axis=1)
+    submit_form(driver)
+
+
+def urls_find(driver, field_name, n_fields):
+    """
+    Find the urls for computers in hp result page.
+    """
+    source = driver.page_source
+    urls = [f"{field_name}[{i}]" for i in range(n_fields)]
+    urls = filter(lambda x: x in source, urls)
+    urls = map(lambda x: driver.find_element(By.ID, x), urls)
+    urls = map(lambda x: x.get_attribute("href"), urls)
+    return urls
+
+
+def set_url(row, sn_urls):
+    serial_number = row["serial_number"]
+    url = sn_urls[serial_number]
+    row["url"] = url
+    return row
+
+
+def append_missing_product_number_to_url(row):
+    url = row["url"]
+    product_number = row["product_number"]
+    if product_number not in url:
+        row["url"] = f"{url}&sgu={product_number}"
+    return row
+
+
+def computer_urls_get(driver, computers):
+    """
+    Retreive urls for each computer.
+    """
+    n_items = len(computers)
+    urls_det = urls_find(driver, "Viewdetails", n_items)
+    urls_opt = urls_find(driver, "ViewOptions", n_items)
+    urls = list(chain(urls_opt, urls_det))
+    sn_urls = {}
+    for url in urls:
+        serial_number = url.split("serialnumber=")[1]
+        sn_urls[serial_number] = url
+    computers = computers.apply(lambda x: set_url(x, sn_urls), axis=1)
+    computers = computers.apply(append_missing_product_number_to_url, axis=1)
+    return computers
+
+
+def query_clear(driver):
+    """
+    Clears hp warranty entries such that information can be entred.
+    Silly workaround, that abuses that clicking "Single product" clears
+    results.
+    """
+    driver.find_element(By.ID, "SingleProduct").click()
+    driver.find_element(By.ID, "MultipleProduct").click()
+
+
+def wait_for_errors_or_success(driver, timeout=300):
+    """
+    Wait for results from query, then test if there is an error
+    with product identification from HP.
+    Returns False if there are any products which cannot be identified.
+    Returns True if products are successfully identified.
+    Will raise a ValueError on any other results.
+    """
+    poll_intervall = 0.5
+    end_time = time.time() + timeout
+    error_msg = "product cannot be identified"
+    summary_msg = "multiple-products-summary"
+    while (time.time() < end_time):
+        time.sleep(poll_intervall)
+        if error_msg in driver.page_source:
+            return False
+        if summary_msg in driver.current_url:
+            return True
+    raise ValueError("Unexpected result in page.")
+
+
+def products_get(driver, computers):
+    """
+    """
+    wait = WebDriverWait(driver, timeout=120)
+    n_items = len(computers)
+
+    driver.get(WARRANTY_URL)
+    query_clear(driver)
+    add_item = driver.find_element(By.ID, "AddItem")
+    for _ in range(n_items - 2):
+        add_item.click()
+    serial_numbers_send(driver, computers)
+    success = wait_for_errors_or_success(driver)
+    if not success:
+        product_numbers_send(driver, computers)
+        wait_for_errors_or_success(driver)
+    computers = computer_urls_get(driver, computers)
+    return computers
 
 
 def hp_warranty_get():
@@ -121,4 +236,4 @@ def hp_warranty_get():
 
 
 if __name__ == "__main__":
-    warranty = HPWarrantyLookup()
+    hp_warranty_get()
