2523a25d9c11655b92541aa2ea3dfe31fc6ab05e
Christopher A <cau005@uit.no>
2024-06-07 12:33:48 +0200

    Creating a function for getting warranty from hp.

--- src/computer.py
+++ src/computer.py
@@ -9,3 +9,26 @@ class Computer:
         self.warranty_end = date.min
         self.url = ""
         self.error = ""
+
+    def warranty_get(self, driver):
+        """
+        Get the warranty for a computer.
+        """
+        warranty_start = None
+        warranty_end = None
+        driver.get(self.url)
+        is_loaded = wait_for_load(driver)
+        if not is_loaded:
+            self.error = "Timeout"
+            return
+        elements = driver.find_elements(By.CLASS_NAME, "label")
+        for element in elements:
+            if element.text == "Start date":
+                warranty_start = date_read(element)
+            if element.text == "End date":
+                warranty_end = date_read(element)
+        if warranty_start == None or warranty_end == None:
+            url = driver.current_url
+            raise ValueError(f"Warranty was not found in current page: {url}")
+        self.warranty_start = warranty_start
+        self.warranty_end = warranty_end
