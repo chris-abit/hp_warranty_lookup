b6e61aa039f7814321bdd5952bd9b4b0869af6e5
Christopher A <cau005@uit.no>
2024-06-13 11:12:07 +0200

    Creating utility function for waiting for page loading.

--- src/utility.py
+++ src/utility.py
@@ -1,3 +1,24 @@
+import time
+
+
+def wait_for_load(driver, timeout=300):
+    """
+    Wait for loading of computer page to finish.
+    """
+    poll_interwall = 0.5
+    end_time = time.time() + timeout
+    load_msg = '"loading-screen-wrapper'
+    error_msg = "Refresh page"
+
+    while time.time() < end_time:
+        time.sleep(poll_interwall)
+        if error_msg in driver.page_source:
+            time.sleep(5)
+            driver.refresh()
+            print(f"Refreshing: {driver.current_url}")
+        elif load_msg not in driver.page_source:
+            return True
+    return False
 
 
 def computers_batched(computers):
