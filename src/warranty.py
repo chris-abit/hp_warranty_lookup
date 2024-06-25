6326335bcef511705caf5e03881a1ee7ef1ae1e8
Christopher A <cau005@uit.no>
2024-06-25 15:13:46 +0200

    Removing unused code.

--- src/warranty.py
+++ src/warranty.py
@@ -1,14 +1,12 @@
 import time
 from pathlib import Path
 from datetime import datetime, date
-from itertools import chain
-import pandas as pd
 from selenium.webdriver import Firefox
 from selenium.common import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from utility import wait_for_load
 from computer import Computer, computers_read, computers_save, computers_batched
+
 
 WARRANTY_URL = "https://support.hp.com/us-en/check-warranty#multiple"
 
@@ -35,27 +33,6 @@ def initialize_browser():
     return driver
 
 
-def wait_for_errors_or_success(driver, timeout=300):
-    """
-    Wait for results from query, then test if there is an error
-    with product identification from HP.
-    Returns False if there are any products which cannot be identified.
-    Returns True if products are successfully identified.
-    Will raise a ValueError on any other results.
-    """
-    poll_intervall = 0.5
-    end_time = time.time() + timeout
-    error_msg = "product cannot be identified"
-    summary_msg = "multiple-products-summary"
-    while (time.time() < end_time):
-        time.sleep(poll_intervall)
-        if error_msg in driver.page_source:
-            return False
-        if summary_msg in driver.current_url:
-            return True
-    raise ValueError("Unexpected result in page.")
-
-
 def batch_warranty_get(driver, computers):
     """
     Get warranties for computers in batch.
@@ -64,7 +41,6 @@ def batch_warranty_get(driver, computers):
     computers = filter(lambda x: x.error == "", computers)
     for computer in computers:
         computer.warranty_get(driver)
-
 
 
 def hp_warranty_get():
